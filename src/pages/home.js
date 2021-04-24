59a53e95258a4398634f64dab3ed098ea7c706ca
Frank <frankamalafronte@gmail.com>
2021-04-24 19:57:42 -0400

    working on it

--- src/pages/home.js
+++ src/pages/home.js
@@ -45,8 +45,8 @@ export default function Home(props) {
         <Img style ={{width:'100%', height:'80vh'}}fluid = {data.racebrook.childImageSharp.fluid}/>
         </div>
       <div id="allEventDetails" className ={styles.eventDetails}>
-      <div className={styles.detail} style={{ paddingTop:'10px', paddingBottom:'50px', fontWeight:'900', fontSize:'40px'}} >
-         Chip Malafronte Memorial Golf Tournament For The Boy
+      <div className={styles.detail} style={{ paddingTop:'10px', paddingBottom:'50px', fontWeight:'900', fontSize:'37px'}} >
+         1st Annual Chip Malafronte <br/> Memorial Golf Tournament 
          </div>
          <div   id ={styles.racebook} onClick={goToRaceBrook} className={styles.racebrookButton}>
           Racebrook Country Club
