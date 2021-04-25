232b6601288db6a330704c6819230fae3bf2553b
rmkBrenner <rmkbrenner@gmail.com>
2021-04-24 20:08:54 -0400

    mobile version

--- src/components/Links.js
+++ src/components/Links.js
@@ -2,27 +2,17 @@ import {Link} from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styles from '../components/styles.module.css';
-import Logo from '../content/images/TBFinal.jpg'
+import LogoM from '../content/images/TBFinal.jpg';
+import LogoD from '../content/images/TBFinal_horizontal.jpg'
 
 export default function Links({children}) {
   return (
     <div>
       <div id= "header" className={styles.nav} >
-<<<<<<< HEAD
         <div id ="header-left" style={{width:'40%'}}>
-        <img src ={Logo} style={{width:'220px', height:'100%'}}/>
-=======
-      <div id ="logo" style={{width:'30%'}}>
-      <img src ={Logo} style={{width:'250px', height:'100%'}}/>
+        {/* <img src ={LogoD} style={{height:'100%'}} className={styles.logoDesktop}/> */}
+        <img src ={LogoM} style={{height:'100%'}} className={styles.logoMobile}/>
       </div>
-      <div id="Blank Space" style={{width:'30%'}}>
-      </div>
-      <div id = "links" style={{ display:'flex',width:'30%',justifyContent:'flex-end'}}>
-         <Link to ="/"><button className={styles.navLink}>Home</button></Link>
-         <Link to ="/chip"><button className={styles.navLink}>Chip's Story</button></Link>
-        <Link to ="https://tournamentfortheboy.square.site"><button style={{ color:'white',backgroundColor:'green', border:'none', borderRadius: '8px'}} className={styles.navLink}>Participate</button> </Link>
->>>>>>> f6f74f65dc065db04a81bda5624306f7c40d3802
-        </div>
      
      {/*<div id="header-center" style={{width:'20%'}}>
         <div style={{fontSize:'30px', paddingTop:'10px', paddingBottom:'10px'}}>
@@ -36,7 +26,7 @@ export default function Links({children}) {
         <div id = "header-right" style={{ display:'flex',width:'50%',justifyContent:'flex-end'}}>
           <Link to ="/"><button className={styles.navLink}>Home</button></Link>
           <Link to ="/chip"><button className={styles.navLink}>Chip's Story</button></Link>
-          <Link to ="https://tournamentfortheboy.square.site"><button style={{ color:'white',backgroundColor:'green', border:'none', borderRadius: '8px'}} className={styles.navLink}>Participate</button> </Link>
+          <Link to ="https://tournamentfortheboy.square.site"><button className={styles.navButton}>Participate</button> </Link>
           </div>
       </div>
 
--- src/pages/chip.js
+++ src/pages/chip.js
@@ -46,15 +46,13 @@ export default function Chip() {
   return (
     <body>
       <Links/>
-      <div className ={styles.heroBottom} style={{paddingTop: '0px', paddingBottom: '0px'}}>Chip's Story</div>
+      <h2>Chip's Story</h2> 
       <div id ="bio" className={styles.weddingParty}>
           <div className={styles.centerWrapper}>
             <div className={styles.center} ><Img className={styles.centerPic} fluid = {data.baby.childImageSharp.fluid} /></div>
             <p>Chip Malafronte was a long-time sports writer for the New Haven Register. He received several awards throughout his career for his exceptional talent in the field, most notably with The Society of Professional Journalism. Chip is best known for his editorial column entitled "Sunday Gravy", combining his excellence in reporting with his sharp wit. His readers looked forward to the personal anecdotes Chip tenderly wove into his weekly articles, many of which featured his young son, John Paul, aka “The Boy”.
-           </p>
-           <br/><br/>
-            <p>A lifelong resident of Orange, Chip was a well-rounded athlete, sports enthusiast and a standout at Amity High School in basketball and baseball. His true passion was baseball which began in Orange Little League and he pursued well into his 30s with the West Haven Twilight League, where he was a Gold Bat recipient in 2007. </p>
-            <p><br/>In addition to his love of sports, Chip was an avid chronicler who took a particular interest in the history of the U.S. Presidents; he visited Mount Rushmore in 2016. Chip was a big fan of music, especially classic rock, and had a special affinity for Van Halen’s album 5150. 
+           <br/><br/>A lifelong resident of Orange, Chip was a well-rounded athlete, sports enthusiast and a standout at Amity High School in basketball and baseball. His true passion was baseball which began in Orange Little League and he pursued well into his 30s with the West Haven Twilight League, where he was a Gold Bat recipient in 2007. 
+           <br/><br/>In addition to his love of sports, Chip was an avid chronicler who took a particular interest in the history of the U.S. Presidents; he visited Mount Rushmore in 2016. Chip was a big fan of music, especially classic rock, and had a special affinity for Van Halen’s album 5150. 
             <br/><br/>Chip is the proud father of John Paul Malafronte, with whom he enjoyed touring National monuments and baseball stadiums across the country. Chip encouraged and coached JP in both baseball and basketball, on the field and at home. The two loved statistics and trivia, playing card games such as cribbage, spending time with family, and hanging out with their beloved dog, Bryce. John Paul continues to play sports and maintains a high academic standing in school. JP will attend Notre Dame High School, West Haven, in the fall. 
             <br/><br/>Good friends were important to Chip and he valued the longtime buddies with whom he grew up. From pick-up games on a Saturday afternoon to just hanging out, these guys shared many laughs together. 
             <br/><br/>Chip was a devoted son, brother, cousin, uncle, colleague, and friend. His tenacity, resolve, and faith in God were inspirational as he courageously defied the odds in his two-year battle with cancer. Chip leaves behind a legacy of good humor, quiet strength, and genuine kindness to all. 
